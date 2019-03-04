224d94eb4ca7f9e868aeaecbbfd814ac23a81908
Vladimir Ice <mr.vladimir.ice@gmail.com>
2019-03-04 11:29:55 +0300

    stats constants has been added

--- src/stats/dictionary/param-types-dictionary.ts
+++ src/stats/dictionary/param-types-dictionary.ts
@@ -1,1 +1,37 @@
-export const USERS_PERSON__NUMBER = 100;
+export const USERS_PERSON__NUMBER                   = 100;
+export const USERS_PERSON__DELTA_PT24H              = 101;
+
+export const ORGS_PERSON__NUMBER                    = 200;
+export const ORGS_PERSON__DELTA_PT24H               = 201;
+
+export const TAGS_PERSON__NUMBER                    = 300;
+export const TAGS_PERSON__DELTA_PT24H               = 301;
+
+
+
+export const COMMENTS_PARENT__NUMBER                = 400;
+export const COMMENTS_PARENT__DELTA_PT24H           = 401;
+
+export const COMMENTS_REPLY__NUMBER                 = 450;
+export const COMMENTS_REPLY__DELTA_PT24H            = 451;
+
+
+
+export const POSTS_MEDIA__NUMBER                    = 500;
+export const POSTS_MEDIA__DELTA_PT24H               = 501;
+
+export const POSTS_DIRECT__NUMBER                   = 550;
+export const POSTS_DIRECT__DELTA_PT24H              = 551;
+
+export const POSTS_REPOST_MEDIA__NUMBER             = 600;
+export const POSTS_REPOST_MEDIA__DELTA_PT24H        = 601;
+
+export const POSTS_REPOST_DIRECT__NUMBER            = 650;
+export const POSTS_REPOST_DIRECT__DELTA_PT24H       = 651;
+
+
+export const ACTIVITIES_VOTE_UPVOTE__NUMBER         = 700;
+export const ACTIVITIES_VOTE_UPVOTE__DELTA_PT24H    = 701;
+
+export const ACTIVITIES_VOTE_DOWNVOTE__NUMBER       = 750;
+export const ACTIVITIES_VOTE_DOWNVOTE__DELTA_PT24H  = 751;
