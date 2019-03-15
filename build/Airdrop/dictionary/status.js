76fd82cac0d8a4853e8492839266f6de33cd1882
dimalitvinov <dima8litvinov@gmail.com>
2019-03-15 18:46:33 +0300

    #111 build airdrop

---
+++ build/Airdrop/dictionary/status.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NEW = 1;
+exports.PENDING = 2;
+exports.RECEIVED = 3;
+exports.EXPIRED = 4;
+exports.ERROR = 5;
+exports.NO_PARTICIPATION = 6;
--- src/Airdrop/dictionary/status.ts
+++ src/Airdrop/dictionary/status.ts
@@ -1,5 +1,6 @@
 export const NEW = 1;
-export const IN_PROGRESS = 2;
+export const PENDING = 2;
 export const RECEIVED = 3;
 export const EXPIRED = 4;
 export const ERROR = 5;
+export const NO_PARTICIPATION = 6;
