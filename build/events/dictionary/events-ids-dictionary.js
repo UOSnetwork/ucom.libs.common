362b0c1da37fc260a83be3a1da14df20df2c0085
vladimirice <mr.vladimir.ice@gmail.com>
2019-06-02 18:53:54 +0300

    update dev deps and add one more event to the dictionary

--- build/events/dictionary/events-ids-dictionary.js
+++ build/events/dictionary/events-ids-dictionary.js
@@ -1,7 +1,11 @@
 "use strict";
+// #task - completely move NotificationsEventIdDictionary from ucom.backend
 class EventsIdsDictionary {
     static registration() {
-        return 130; // #task - completely move NotificationsEventIdDictionary from ucom.backend
+        return 130;
+    }
+    static referral() {
+        return 140;
     }
 }
 module.exports = EventsIdsDictionary;
--- src/events/dictionary/events-ids-dictionary.ts
+++ src/events/dictionary/events-ids-dictionary.ts
@@ -1,6 +1,12 @@
+// #task - completely move NotificationsEventIdDictionary from ucom.backend
+
 class EventsIdsDictionary {
   public static registration(): number {
-    return 130; // #task - completely move NotificationsEventIdDictionary from ucom.backend
+    return 130;
+  }
+
+  public static referral(): number {
+    return 140;
   }
 }
 
