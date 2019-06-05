70908ed4e27e685b637b94c3bc68239d4e75274e
vladimirice <mr.vladimir.ice@gmail.com>
2019-06-05 22:47:32 +0300

    add EntityNames dictionary

--- build/common/dictionary/entity-names.js
+++ build/common/dictionary/entity-names.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.USERS = 'users     ';
 exports.POSTS = 'posts     ';
-exports.COMMENTS = 'posts     ';
+exports.COMMENTS = 'comments  ';
 exports.ORGANIZATIONS = 'org       ';
 exports.TAGS = 'tags      ';
 exports.BLOCKCHAIN_NODES = 'bl_nodes  ';
