4d170e032791a9574aea8ca04adcf92a5ace4efc
vladimirice <mr.vladimir.ice@gmail.com>
2019-06-06 18:35:56 +0300

    fix post types namings

--- src/posts/dictionary/post-types.ts
+++ src/posts/dictionary/post-types.ts
@@ -1,4 +1,4 @@
-export const MEDIA_POST  = 1; // Business name is - Publication
-export const OFFER       = 2;
-export const DIRECT_POST = 10; // Business name is - post
-export const REPOST      = 11;
+export const MEDIA  = 1; // Business name is - Publication
+export const OFFER  = 2;
+export const DIRECT = 10; // Business name is - post
+export const REPOST = 11;
