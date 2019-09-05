1cba40d5c784758d4119908e8f281507d848ef32
vladimirice <mr.vladimir.ice@gmail.com>
2019-09-05 15:57:20 +0300

    add ts declarations and ContentTypesDictionary

--- build/common/dictionary/content-types-dictionary.d.ts
+++ build/common/dictionary/content-types-dictionary.d.ts
@@ -1,9 +1,14 @@
 declare class ContentTypesDictionary {
-    static getTypeMediaPost(): number;
-    static getTypeOffer(): number;
-    static getTypeComment(): number;
-    static getTypeOrganization(): number;
-    static getTypeDirectPost(): number;
-    static getTypeRepost(): number;
+  static getTypeMediaPost(): number;
+
+  static getTypeOffer(): number;
+
+  static getTypeComment(): number;
+
+  static getTypeOrganization(): number;
+
+  static getTypeDirectPost(): number;
+
+  static getTypeRepost(): number;
 }
-export { ContentTypesDictionary, };
+export { ContentTypesDictionary };
--- build/events/dictionary/events-ids-dictionary.d.ts
+++ build/events/dictionary/events-ids-dictionary.d.ts
@@ -1,16 +1,28 @@
 declare class EventsIdsDictionary {
-    static userCreatesDirectPostForOtherUser(): number;
-    static registration(): number;
-    static referral(): number;
-    static userCreatesProfile(): number;
-    static userUpdatesProfile(): number;
-    static userCreatesMediaPostFromHimself(): number;
-    static userUpdatesMediaPostFromHimself(): number;
-    static userUpdatesDirectPostForUser(): number;
-    static userCreatesMediaPostFromOrganization(): number;
-    static userUpdatesMediaPostFromOrganization(): number;
-    static userUpdatesDirectPostForOrganization(): number;
-    static userCreatesOrganization(): number;
-    static userUpdatesOrganization(): number;
+  static userCreatesDirectPostForOtherUser(): number;
+
+  static registration(): number;
+
+  static referral(): number;
+
+  static userCreatesProfile(): number;
+
+  static userUpdatesProfile(): number;
+
+  static userCreatesMediaPostFromHimself(): number;
+
+  static userUpdatesMediaPostFromHimself(): number;
+
+  static userUpdatesDirectPostForUser(): number;
+
+  static userCreatesMediaPostFromOrganization(): number;
+
+  static userUpdatesMediaPostFromOrganization(): number;
+
+  static userUpdatesDirectPostForOrganization(): number;
+
+  static userCreatesOrganization(): number;
+
+  static userUpdatesOrganization(): number;
 }
 export = EventsIdsDictionary;
