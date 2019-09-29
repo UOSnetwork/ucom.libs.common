3d0d3dd9d4fedcc4ea3c465c2d395176d4a89dae
vladimirice <mr.vladimir.ice@gmail.com>
2019-09-29 14:33:43 +0300

    add two update comment eventIds

--- build/common/dictionary/content-types-dictionary.d.ts
+++ build/common/dictionary/content-types-dictionary.d.ts
@@ -1,16 +1,10 @@
 declare class ContentTypesDictionary {
-  static getTypeMediaPost(): number;
-
-  static getTypeOffer(): number;
-
-  static getTypeComment(): number;
-
-  static getTypeOrganization(): number;
-
-  static getTypeDirectPost(): number;
-
-  static getTypeRepost(): number;
-
-  static getTypeAutoUpdate(): number;
+    static getTypeMediaPost(): number;
+    static getTypeOffer(): number;
+    static getTypeComment(): number;
+    static getTypeOrganization(): number;
+    static getTypeDirectPost(): number;
+    static getTypeRepost(): number;
+    static getTypeAutoUpdate(): number;
 }
 export = ContentTypesDictionary;
